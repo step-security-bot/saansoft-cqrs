78a2d54b06a7d28bc1e0a0522f8e3e32918e986e
Sandra Greenhalgh <sandragreenhalgh@gmail.com>
2024-03-22 14:24:16 +0800

    ci: treat warnings as errors (#3)
    **Description:**
    
    Update project settings so that compiler warnings are treated as errors
    
    
    **Check list:**
    
    - [ ] Mark if documentation changes are required
    - [ ] Mark if tests were added or updated to cover the changes

--- src/SaanSoft.Cqrs.Core/Class1.cs
+++ src/SaanSoft.Cqrs.Core/Class1.cs
@@ -2,8 +2,22 @@
 
 public class Class1
 {
+    public Class1()
+    {
+    }
+
     public void Method1()
     {
         Console.WriteLine("Hello World");
+
+        var testWarningsAsErrors = new TestClass
+        {
+            Name = "Bob"
+        };
+    }
+
+    public class TestClass
+    {
+        public required string Name { get; set; }
     }
 }
