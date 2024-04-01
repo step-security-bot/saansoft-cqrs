3d15dfcfeb07a1b7361fadd3eb2811bbc62968f1
Sandra Greenhalgh <sandragreenhalgh@gmail.com>
2024-04-01 15:34:00 +0800

    ci: add codec overage to unit test project and github actions (#56)
    Signed-off-by: Sandra Greenhalgh <sandragreenhalgh@gmail.com>

--- src/SaanSoft.Cqrs/Bus/IQueryBus.cs
+++ src/SaanSoft.Cqrs/Bus/IQueryBus.cs
@@ -4,6 +4,16 @@ namespace SaanSoft.Cqrs.Bus;
 
 public interface IQueryBus<TMessageId>
 {
+    /// <summary>
+    /// Send a query for information
+    /// </summary>
+    /// <param name="query"></param>
+    /// <typeparam name="TQuery"></typeparam>
+    /// <typeparam name="TResult">
+    ///     Contains the payload result of the query.
+    ///     Also contains information on if the query was successful or not, and error messages.
+    /// </typeparam>
+    /// <returns></returns>
     Task<TResult> QueryAsync<TQuery, TResult>(IQuery<TQuery, TResult> query)
         where TQuery : IQuery<TMessageId, TQuery, TResult>
         where TResult : IQueryResult;
